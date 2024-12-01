6d6f5fc4e15cf43a781e2d02bf8a7efa9a38b4d2
kishu279 <souravpoddar6677@gmail.com>
2024-12-01 11:20:39 +0530

    random

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+    
 const app = express();
+
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Hello World");
+});
+
+app.post("/signup", (req, res) => {
+    try{
+        const {username, password} = req.body;
+
+
+
+
+
+    } catch(error) {
+        console.log(error);
+    }
 });
 
 app.listen(3000);
