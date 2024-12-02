a895641f417ddd0899d4b873c0af4c92fb271221
kishu279 <souravpoddar6677@gmail.com>
2024-12-03 03:50:34 +0530

    delete todos added

--- UserRoutes/userTodo.js
+++ UserRoutes/userTodo.js
@@ -72,4 +72,42 @@ const updateTodos = async (req, res) => {
 
 router.put("/updatetodos/:isDone", updateTodos);
 
+const deletetodos = async (req, res) => {
+  try {
+    // Find the todo by title
+    const todo = await Todo.findOne({ title: req.params.title });
+
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found",
+      });
+    }
+
+    // Proceed to delete the todo by its ID
+    const result = await Todo.findByIdAndDelete(todo._id);
+
+    // Check if the deletion was successful
+    if (result) {
+      return res.status(200).json({
+        success: true,
+        message: "Todo deleted successfully",
+      });
+    }
+
+    return res.status(400).json({
+      success: false,
+      message: "Failed to delete the todo",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while deleting the todo",
+      error: error.message,
+    });
+  }
+};
+
+router.delete("/deletetodos/:title", deletetodos);
+
 module.exports = router;
